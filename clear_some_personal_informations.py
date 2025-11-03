ddf6d19b3c88176cc562fbef5284c44d1d60a1fb
congmingyige <1249591860@qq.com>
2025-11-03 08:59:42 +0800

    too long version

--- clear_some_personal_informations.py
+++ clear_some_personal_informations.py
@@ -1,38 +1,64 @@
 from PIL import Image
-import cv2
-import matplotlib.pyplot as plt    # 显示PIL.Image读取的图片
 import numpy as np
 
-'''
-image1 = cv2.imread(r'C:/Users/chenguanbin/Latex_Profile_CS_and_OI/陈冠斌_个人简历_2025年6月毕业_CS_Page1.png') 
-print('type(image1):', type(image1))    # type(image1): <class 'numpy.ndarray'>
-print(image1.shape)    # (109, 992, 3)
-print(image1.dtype)    # uint8
-'''
-
-img1 = Image.open('陈冠斌_个人简历_2025年6月毕业_CS_Page1.png')
-# print(type(img1))
+# 读取图片
+img1 = Image.open('陈冠斌_个人简历_Page1.jpg')
 img = np.array(img1, dtype=np.uint8)
-# print(type(img))
-# print(img.shape)
 
-value = 255 # 0
-ratio=1.42
-img[0:int(212*ratio), 0:int(640*ratio), :] = value
-img[int(217*ratio):int(255*ratio), 0:int(800*ratio), :] = value
-img[int(63*ratio):int(257*ratio), int(849*ratio):int(1058*ratio), :] = value
+# 图片处理（覆盖区域）
+value = 255  # 填充白色
 
+img[0:360, 0:1800, :] = value
+# img[217:255, 0:800, :] = value
+# img[63:257, 849:1058, :] = value
+
+# ratio = 1.5
+# img[0:int(212*ratio), 0:int(640*ratio), :] = value
+# img[int(217*ratio):int(255*ratio), 0:int(800*ratio), :] = value
+# img[int(63*ratio):int(257*ratio), int(849*ratio):int(1058*ratio), :] = value
+
+# 转换回PIL图片并保存
 img_new = Image.fromarray(img)
+img_new.save('陈冠斌_个人简历_Page1.jpg_auto.jpg')
 
-img_new.save('陈冠斌_个人简历_2025年6月毕业_CS_Page1_PS_auto.png')
+# 用系统默认查看器显示处理后的图片
+img_new.show()
+
+# from PIL import Image
+# import cv2
+# import matplotlib.pyplot as plt    # 显示PIL.Image读取的图片
+# import numpy as np
+
+# '''
+# image1 = cv2.imread(r'C:/Users/chenguanbin/Latex_Profile_CS_and_OI/陈冠斌_个人简历_2025年6月毕业_CS_Page1.png') 
+# print('type(image1):', type(image1))    # type(image1): <class 'numpy.ndarray'>
+# print(image1.shape)    # (109, 992, 3)
+# print(image1.dtype)    # uint8
+# '''
+
+# img1 = Image.open('陈冠斌_个人简历_Page1.jpg')
+# # print(type(img1))
+# img = np.array(img1, dtype=np.uint8)
+# # print(type(img))
+# # print(img.shape)
+
+# value = 255 # 0
+# ratio=1.42
+# img[0:int(212*ratio), 0:int(640*ratio), :] = value
+# img[int(217*ratio):int(255*ratio), 0:int(800*ratio), :] = value
+# img[int(63*ratio):int(257*ratio), int(849*ratio):int(1058*ratio), :] = value
+
+# img_new = Image.fromarray(img)
+
+# img_new.save('陈冠斌_个人简历_Page1.jpg_auto.jpg')
 
 
 
 
-'''
-620
-209
+# '''
+# 620
+# 209
 
-805
-217-255
-'''
+# 805
+# 217-255
+# '''
