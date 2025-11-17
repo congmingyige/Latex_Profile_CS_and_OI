4f413837cc19bab34d4ff49d2bbe5dbc204198ff
congmingyige <1249591860@qq.com>
2025-11-17 14:45:01 +0800

    feat:5 standard resume pdf

--- clear_some_personal_informations.py
+++ clear_some_personal_informations.py
@@ -2,15 +2,20 @@ from PIL import Image
 import numpy as np
 
 # 读取图片
-img1 = Image.open('陈冠斌_个人简历.jpg')
+# img1 = Image.open('陈冠斌_自动驾驶算法.jpg')
+image_name = '陈冠斌_自动驾驶算法.jpg'
+# image_name = '陈冠斌_C++研发工程师_智驾_地图_Page1.jpg'
+img1 = Image.open(image_name)
 img = np.array(img1, dtype=np.uint8)
+image_name_new = image_name[:-4] + '_auto.jpg'
 
 # 图片处理（覆盖区域）
 value = 255  # 填充白色
 
 # img[0:360, 0:1800, :] = value
 
-img[0:540, 0:1800, :] = value
+img[0:210, 0:1800, :] = value
+# img[0:250, 0:1800, :] = value
 
 
 # img[217:255, 0:800, :] = value
@@ -23,7 +28,7 @@ img[0:540, 0:1800, :] = value
 
 # 转换回PIL图片并保存
 img_new = Image.fromarray(img)
-img_new.save('陈冠斌_个人简历_Page1.jpg_auto.jpg')
+img_new.save(image_name_new)
 
 # 用系统默认查看器显示处理后的图片
 img_new.show()
